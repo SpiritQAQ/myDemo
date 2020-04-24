d858cdd94ad0d45cc42630057877e9056d8377c6
Zegel <spiritqaq@gmail.com>
2020-04-24 12:29:05 +0800

    new test

--- js/4_eventLoop.js
+++ js/4_eventLoop.js
@@ -1,36 +1,66 @@
-console.log('1');
+// test1
+// console.log('1');
+
+// setTimeout(function () {
+//   console.log('2');
+//   process.nextTick(function () {
+//     console.log('3');
+//   })
+//   new Promise(function (resolve) {
+//     console.log('4');
+//     resolve();
+//   }).then(function () {
+//     console.log('5')
+//   })
+// })
+// process.nextTick(function () {
+//   console.log('6');
+// })
+// new Promise(function (resolve) {
+//   console.log('7');
+//   resolve();
+// }).then(function () {
+//   console.log('8')
+// })
+
+// setTimeout(function () {
+//   console.log('9');
+//   process.nextTick(function () {
+//     console.log('10');
+//   })
+//   new Promise(function (resolve) {
+//     console.log('11');
+//     resolve();
+//   }).then(function () {
+//     console.log('12')
+//   })
+// })
+
+// texst2
+console.log('script start')
+
+async function async1() {
+  await async2()
+  console.log('async1 end')
+}
+async function async2() {
+  console.log('async2 end')
+}
+async1()
 
 setTimeout(function () {
-  console.log('2');
-  process.nextTick(function () {
-    console.log('3');
-  })
-  new Promise(function (resolve) {
-    console.log('4');
-    resolve();
-  }).then(function () {
-    console.log('5')
-  })
-})
-process.nextTick(function () {
-  console.log('6');
-})
-new Promise(function (resolve) {
-  console.log('7');
-  resolve();
-}).then(function () {
-  console.log('8')
-})
+  console.log('setTimeout')
+}, 0)
 
-setTimeout(function () {
-  console.log('9');
-  process.nextTick(function () {
-    console.log('10');
-  })
-  new Promise(function (resolve) {
-    console.log('11');
-    resolve();
-  }).then(function () {
-    console.log('12')
-  })
+new Promise(resolve => {
+  console.log('Promise')
+  resolve()
 })
+  .then(function () {
+    console.log('promise1')
+  })
+  .then(function () {
+    console.log('promise2')
+  })
+
+console.log('script end')
