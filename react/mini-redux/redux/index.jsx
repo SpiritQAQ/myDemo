63b2243bd6b00d03195c554aa707fe2029160ec2
Zegel <spiritqaq@gmail.com>
2022-05-10 19:24:33 +0800

    将store的api改成redux一样的

--- react/mini-redux/redux/index.jsx
+++ react/mini-redux/redux/index.jsx
@@ -5,19 +5,27 @@ import _ from 'lodash'
  * dispatch用来规范setState的流程包括reduce流程 dispatch是来自react-redux
  *
  */
-const store = {
-  state: {},
-  listener: [],
-  reducer: undefined,
 
-  setState: (newState) => {
-    store.state = newState
-    store.listener.forEach((fn) => fn(newState))
+let state = {}
+let reducer = undefined
+let listener = []
+const setState = (newState) => {
+  state = newState
+  listener.forEach((fn) => fn(newState))
+}
+
+const store = {
+  getState: () => state,
+
+  dispatch: (action) => {
+    const newState = reducer?.(state, action)
+    setState(newState)
   },
+
   subscribe: (fn) => {
-    store.listener.push(fn)
+    listener.push(fn)
     return () => {
-      store.listener.splice(store.listener.findIndex(fn), 1)
+      listener.splice(listener.findIndex(fn), 1)
     }
   },
 }
@@ -30,7 +38,7 @@ const store = {
 // 是为了第一步可以封装,直接调用connectToTag(TagList)
 export const connect = (selector, mapDispatchToProps) => (Component) => {
   return (componentProps) => {
-    const { state, setState, subscribe, reducer } = store
+    const { subscribe, dispatch } = store
 
     const data = selector ? selector(state) : state
 
@@ -39,7 +47,8 @@ export const connect = (selector, mapDispatchToProps) => (Component) => {
       // 首次渲染，订阅store变化
       // 变化发生的时候通过setState触发connect重新渲染
       subscribe(() => {
-        const newData = selector ? selector(store.state) : store.state
+        const newData = selector ? selector(state) : state
+
         // 判断命名空间内数据是否变化，来控制是否重新渲染
         if (!_.isEqual(data, newData)) {
           update({})
@@ -49,12 +58,6 @@ export const connect = (selector, mapDispatchToProps) => (Component) => {
 
       // 如果seletor变化，会造成重复订阅
     }, [selector])
-
-    const dispatch = (action) => {
-      const newState = reducer(state, action)
-
-      setState(newState)
-    }
 
     const thePropsFromMapDispatch = mapDispatchToProps
       ? mapDispatchToProps(dispatch)
@@ -71,9 +74,9 @@ export const connect = (selector, mapDispatchToProps) => (Component) => {
   }
 }
 
-export const createStore = (reducer, initState) => {
-  store.reducer = reducer
-  store.state = initState
+export const createStore = (_reducer, initState) => {
+  reducer = _reducer
+  state = initState
   return store
 }
 export const AppContext = React.createContext(null)
