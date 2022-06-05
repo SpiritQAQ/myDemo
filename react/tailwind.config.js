c076e5fbd83fb1d506b8de57ca79c7b393b56837
Zegel <spiritqaq@gmail.com>
2022-06-05 23:03:33 +0800

    chore: +tailwind +eslint +prettier

---
+++ react/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}',
+    './grid-demo/index.html', './grid-demo/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
